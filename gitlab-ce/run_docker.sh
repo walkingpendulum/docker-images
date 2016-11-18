c35dbd46c8422da95faf298fade923451181e7e1
Oleg Yunin <oayunin@gmail.com>
2016-11-18 21:09:43 +0000

    add run docker script for gitlab wuth volumes

--- gitlab-ce/run_docker.sh
+++ gitlab-ce/run_docker.sh
@@ -5,8 +5,6 @@ PATH_TO_VOLUME_FOLDER="$DOCKER_VOLUMES_STORAGE_PATH_PREFIX/gitlab-ce"
 sudo docker run --detach \
     --publish 443:443 --publish 81:80 --publish 23:22 \
     --name gitlab-ce \
-    --env GITLAB_OMNIBUS_CONFIG="external_url 'http://gitlab.aeschylus.gr/';" \
-    --hostname gitlab.aeschylus.gr \
     --restart always \
     --volume "$PATH_TO_VOLUME_FOLDER/config":"/etc/gitlab" \
     --volume "$PATH_TO_VOLUME_FOLDER/logs":"/var/log/gitlab" \
